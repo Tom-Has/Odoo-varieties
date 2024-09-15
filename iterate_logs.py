45ca231187ef316b1050c89aa9c54dbb0a15c4af
Tom-Has <t0m.has@pm.me>
2024-09-15 13:37:40 +0200

    Update iterate_logs.py
    remove customer instances and redundant instructions

--- iterate_logs.py
+++ iterate_logs.py
@@ -4,8 +4,7 @@ import os
 import re
 
 counter = 0
-#search_string = '154.54.249.214'
-search_string_list = ['@eu-central-1.amazonses.com']
+search_string_list = ['example']
 
 for file in os.listdir():
     filename = os.fsdecode(file)
@@ -14,7 +13,6 @@ for file in os.listdir():
         for log in zipped:
             with gzip.open(log, 'rt') as f:
                 for line in f:
-                    #if search_string in line:
                     if any(map(line.__contains__, search_string_list)):
                         counter = counter + 1
             if counter > 0:
@@ -23,12 +21,3 @@ for file in os.listdir():
 
 #navigate into /log directory before opening terminal or supply os.listdir() with correct path
 
-"""
-"""
-
-counter = 0
-with open('odoo.log', 'rt') as log:
-    for line in log:
-        if any(map(line.__contains__, search_string_list)):
-            counter = counter + 1
-print(str(log) + " has " + str(counter) + " occurrences of " + str(search_string_list) + ".")
